0371620e2a4da9c54317e144ab630c061e0029ec
brinkqiang <brink.qiang@gmail.com>
2021-01-05 17:14:48 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -5,23 +5,24 @@ local count = 0
 local test = "this is test"
 
 function main_idle()
-
+    print("hello main_idle");
 end
 
-timer:settimer(0, 1, function()
-    main_idle()
+timer:settimer(0, 1, function(id)
+    timer:killtimer(id);
+    main_idle();
 end
 )
 
-timer:settimer(1, 1000, function()
+timer:settimer(1, 1000, function(id)
         count=count+1;
         print("hello wolrd " .. count);
         if count >= 10 then
-            timer:killtimer(1);
+            timer:killtimer(id);
             timer:killall();
             timer:stop();
         end
     end
 )
 
-timer:run()
+timer:run();
