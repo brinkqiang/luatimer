dce7ba3a688650296c9e17fb30113582144bd0ef
brinkqiang <brink.qiang@gmail.com>
2021-01-12 19:27:45 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -8,13 +8,13 @@ function main_idle()
     print("hello main_idle");
 end
 
-timer:settimer(0, 1, function(id)
+timer:settimer(1001, 1, function(id)
     timer:killtimer(id);
     main_idle();
 end
 )
 
-timer:settimer(1, 1000, function(id)
+timer:settimer(1002, 1000, function(id)
         count=count+1;
         print("hello wolrd " .. count);
         if count >= 10 then
